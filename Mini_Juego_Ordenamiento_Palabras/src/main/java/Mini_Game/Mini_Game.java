4d2981df8a6c6bf209b849d9e2808b291d4495f0
unknown <estiven.carmona@udea.edu.co>
2023-03-29 14:58:38 -0500

    Corrección Mini Juego Ordenamiento palabras

---
+++ Mini_Juego_Ordenamiento_Palabras/src/main/java/Mini_Game/Mini_Game.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Mini_Game;
+
+import Views.Init;
+import java.io.IOException;
+
+/**
+ *
+ * @author Alisser
+ */
+public class Mini_Game {
+
+    public static void main(String[] args) throws IOException {
+        Init initWindow = new Init();
+        initWindow.setVisible(true);
+        initWindow.setLocationRelativeTo(null);
+    }
+}
