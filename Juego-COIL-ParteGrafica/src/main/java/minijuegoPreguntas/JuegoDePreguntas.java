4d7fe7a1abc2b110b9a471f25f651248936e21b7
BrahiamAcosta <brahiam.acosta@udea.edu.co>
2023-04-14 08:08:06 -0500

    Modificacion juego 1 e implementacion desde parte grafica

--- Juego-COIL-ParteGrafica/src/main/java/minijuegoPreguntas/JuegoDePreguntas.java
+++ Juego-COIL-ParteGrafica/src/main/java/minijuegoPreguntas/JuegoDePreguntas.java
@@ -1,49 +1,53 @@
 package minijuegoPreguntas;
 
+/**
+ *
+ * @author Brahiam
+ */
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Collections;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 
 public class JuegoDePreguntas extends javax.swing.JFrame {
-    static String textoBaseDePreguntas = "Seleccione la frase que use correctamente el Simple Present"
-            + "\tShe walks\tI walks\tWe walks\tHe walk"
-            +"\n"
-            +"Seleccione la forma correcta de redactar:"
-            + "¿Ella vive aquí?\tDoes she live here?\tShe is live here?\tIs she live here?\tDoes she lives here?";
+    int aciertos = 0;
+    static String urlBD = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRzzKW7lD3dJtG2JUU0OO81H7f5ytB8AGh-VBUKC8CspJjTQDYiUPwicRqKQepUQpQHOiMPBp--Kt11/pub?output=tsv";
+    static String textoBaseDePreguntas = LeerArchivo_ASCII(urlBD);
     static String[] renglones = textoBaseDePreguntas.split("\n");
-    static int cantidadDePreguntas = renglones.length;
+    static int cantidadDePreguntas = 10;
     
     static String[][] baseDePreguntas = new String[cantidadDePreguntas][5];
+    
     String[] preguntaEscogida;
     String pregunta;
     String respuesta;
+    String img;
     ArrayList<String> Opciones = new ArrayList<>();
     int n_pregunta = 0;
-    
-    public static void main(String args[]) {
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        }catch (Exception ex) {
-        }
-        for(int i = 0;i<renglones.length;i++){
-            String renglon = renglones[i];
-            baseDePreguntas[i] = renglon.split("\t");
-        }
-        new JuegoDePreguntas();  
-    }
+
     
     public void escogerPregunta(int n) {
         preguntaEscogida = baseDePreguntas[n];
         pregunta = preguntaEscogida[0];
         respuesta = preguntaEscogida[1];
+        if (preguntaEscogida.length > 5) {
+            img = preguntaEscogida[5];
+        } else {
+            img = "";
+        }
         Opciones.clear();
-        for(int i=1;i<preguntaEscogida.length;i++){
+        for(int i=1;i<preguntaEscogida.length-1;i++){
             Opciones.add(preguntaEscogida[i]);
         }
         for (int i = 0; i < 4; i++) {
@@ -53,6 +57,20 @@ public class JuegoDePreguntas extends javax.swing.JFrame {
     
     public void mostrarPregunta() {
         jLabel1.setText(pregunta);
+        if (img.equals("")) {
+            jLabel2.setVisible(false);
+        } else {
+            jLabel2.setVisible(true);
+            jLabel2.setText("");
+            try {
+                BufferedImage imagen = ImageIO.read(new URL(img));
+                Image imagenEscalada = imagen.getScaledInstance(-1, 260, Image.SCALE_SMOOTH);
+                jLabel2.setIcon(new ImageIcon(imagenEscalada));
+            } catch (IOException e) {
+                jLabel2.setText("La imágen no se pudo cargar");
+                jLabel2.setIcon(null);
+            }
+        }
         jButton1.setText(Opciones.get(0));
         jButton2.setText(Opciones.get(1));
         jButton3.setText(Opciones.get(2));
@@ -67,6 +85,7 @@ public class JuegoDePreguntas extends javax.swing.JFrame {
                     "Muy bien :)",
                     JOptionPane.INFORMATION_MESSAGE
             );
+            aciertos++;
         } else {
             JOptionPane.showMessageDialog(
                     this,
@@ -82,8 +101,8 @@ public class JuegoDePreguntas extends javax.swing.JFrame {
         if (n_pregunta == cantidadDePreguntas) {
             JOptionPane.showMessageDialog(
                     this,
+                    "Puntuacion: "+aciertos+"/10",
                     "Juego Terminado",
-                    "Muy bien :)",
                     JOptionPane.PLAIN_MESSAGE
             );
             System.exit(0);
@@ -94,8 +113,12 @@ public class JuegoDePreguntas extends javax.swing.JFrame {
     }
     
     public JuegoDePreguntas() {
+        for(int i = 0;i<renglones.length;i++){
+            String renglon = renglones[i];
+            baseDePreguntas[i] = renglon.split("\t");
+        }
         initComponents();
-        setSize(600, 600);
+        setSize(600, 520);
         setLocationRelativeTo(null);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -103,8 +126,42 @@ public class JuegoDePreguntas extends javax.swing.JFrame {
         jugar();
     }    
 
+     public static String LeerArchivo_ASCII(String ruta) {
+        try {
+            if (ruta == null) {
+                throw new RuntimeException("Error, la URL de lectura no puede ser nula");
+            }
+            URL url = new URL(ruta);
+            URLConnection conexión = url.openConnection();
+            InputStreamReader isr = new InputStreamReader(conexión.getInputStream());
+            return LeerArchivo_ASCII(isr);
+        } catch (Exception e) {
+            System.out.println("No hay conexión a internet, la base de datos no pudo ser cargada");
+            System.exit(0);
+        }
+        return "";
+    }
+
+    public static String LeerArchivo_ASCII(Reader reader) throws Exception {
+        BufferedReader br = new BufferedReader(reader);
+        String texto = "";
+        String linea;
+        boolean primerRenglón = true;
+        while ((linea = br.readLine()) != null) {
+            if (primerRenglón) {
+                primerRenglón = false;
+            } else {
+                texto += "\n";
+            }
+            texto += linea;
+        }
+        reader.close();
+        br.close();
+        return texto;
+    }
+    
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
@@ -112,14 +169,17 @@ public class JuegoDePreguntas extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Gill Sans Ultra Bold Condensed", 0, 24)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Pregunta Con Imagen");
+        jLabel1.setText("Pregunta con imagen");
 
-        jButton1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jButton1.setBackground(new java.awt.Color(204, 204, 204));
+        jButton1.setFont(new java.awt.Font("Gill Sans Ultra Bold Condensed", 0, 18)); // NOI18N
         jButton1.setText("jButton1");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -127,7 +187,8 @@ public class JuegoDePreguntas extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jButton2.setBackground(new java.awt.Color(204, 204, 204));
+        jButton2.setFont(new java.awt.Font("Gill Sans Ultra Bold Condensed", 0, 18)); // NOI18N
         jButton2.setText("jButton2");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -135,7 +196,8 @@ public class JuegoDePreguntas extends javax.swing.JFrame {
             }
         });
 
-        jButton3.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jButton3.setBackground(new java.awt.Color(204, 204, 204));
+        jButton3.setFont(new java.awt.Font("Gill Sans Ultra Bold Condensed", 0, 18)); // NOI18N
         jButton3.setText("jButton3");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -143,7 +205,8 @@ public class JuegoDePreguntas extends javax.swing.JFrame {
             }
         });
 
-        jButton4.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jButton4.setBackground(new java.awt.Color(204, 204, 204));
+        jButton4.setFont(new java.awt.Font("Gill Sans Ultra Bold Condensed", 0, 18)); // NOI18N
         jButton4.setText("jButton4");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -155,24 +218,26 @@ public class JuegoDePreguntas extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 586, javax.swing.GroupLayout.PREFERRED_SIZE)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 580, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 8, Short.MAX_VALUE))
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 572, Short.MAX_VALUE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(83, 83, 83)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(21, 21, 21)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 281, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(36, 36, 36)
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 239, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton2)
@@ -180,35 +245,36 @@ public class JuegoDePreguntas extends javax.swing.JFrame {
                 .addComponent(jButton3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton4)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(8, 8, 8))
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }// </editor-fold>                        
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
         escogerRespuesta(0);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }                                        
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
         escogerRespuesta(1);
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }                                        
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {                                         
         escogerRespuesta(2);
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }                                        
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {                                         
         escogerRespuesta(3);
-    }//GEN-LAST:event_jButton4ActionPerformed
+    }                                        
 
     
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // Variables declaration - do not modify                     
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
-    // End of variables declaration//GEN-END:variables
+    private javax.swing.JLabel jLabel2;
+    // End of variables declaration                   
 }
 
